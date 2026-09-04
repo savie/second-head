15f733626ab540ef059e1df5bab2990f29ccf42e
savie <ntop_one@yahoo.co.id>
2026-09-04 23:35:51 +0700

    chore(profile): clean data privacy widget imports

--- app/lib/features/profile/data_privacy/data_privacy_widgets.dart
+++ app/lib/features/profile/data_privacy/data_privacy_widgets.dart
@@ -1,15 +1,6 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:share_plus/share_plus.dart';
 import '../../../core/navigation/sh_navigation_shell.dart';
 import '../../../core/theme/sh_theme.dart';
-import '../../../core/storage/storage_service.dart';
-import '../../../core/storage/recovery_snapshot_store.dart';
-import '../../../core/state/sh_profile_state.dart';
-import '../../journey/journey_data.dart';
 
 class DPSubPage extends StatelessWidget {
   const DPSubPage({required this.title, required this.children});
