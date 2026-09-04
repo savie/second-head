fb65c98bac5337eb8283712676cb2ff276db7c1f
savie <ntop_one@yahoo.co.id>
2026-09-05 05:10:41 +0700

    chore(refactor): remove unused account import

--- app/lib/features/profile/account/account_view.dart
+++ app/lib/features/profile/account/account_view.dart
@@ -3,7 +3,6 @@ import 'package:image_picker/image_picker.dart';
 import '../../../core/navigation/sh_navigation_shell.dart';
 import '../../../core/state/sh_profile_state.dart';
 import '../../../core/theme/sh_theme.dart';
-import '../../../core/widgets/sh_brand_mark.dart';
 import '../profile_widgets.dart';
 
 part 'account_widgets.dart';
