0f72c4463465c181fc85b3d3f3cf3314f3880afb
savie <ntop_one@yahoo.co.id>
2026-09-05 09:23:17 +0700

    Import OAuth launch mode dependency

--- app/lib/features/auth/auth_service.dart
+++ app/lib/features/auth/auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../core/identity/sh_identity.dart';
 import '../../core/supabase/supabase_client.dart';
