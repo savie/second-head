6a8073d13f3d7c9a5bdfe89f7480d646b8d51c1f
savie <ntop_one@yahoo.co.id>
2026-09-08 05:45:51 +0700

    reconstruct migration 115 from Supabase DEV

---
+++ database/migrations/20260822051922_p1_revoke_client_event_trigger_execute_final.sql
@@ -0,0 +1,1 @@
+REVOKE ALL ON FUNCTION public.rls_auto_enable() FROM PUBLIC; REVOKE ALL ON FUNCTION public.rls_auto_enable() FROM anon; REVOKE ALL ON FUNCTION public.rls_auto_enable() FROM authenticated; GRANT EXECUTE ON FUNCTION public.rls_auto_enable() TO postgres;
