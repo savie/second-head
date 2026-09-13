2560c553653e46889049d80f13103f9da7ad7336
savie <ntop_one@yahoo.co.id>
2026-09-13 19:04:07 +0700

    Route Experience domain UI through runtime authority

--- app/lib/features/journey/experience/experience_view.dart
+++ app/lib/features/journey/experience/experience_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import '../semantic_runtime_domain_view.dart';
 import '../semantic_domain_view.dart';
 
 class ExperienceView extends StatelessWidget {
   const ExperienceView({super.key});
 
   @override
-  Widget build(BuildContext context) => const SemanticDomainView(domain: ShSemanticDomain.experience);
+  Widget build(BuildContext context) => const SemanticRuntimeDomainView(domain: ShSemanticDomain.experience);
 }
