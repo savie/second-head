a1e41479b3a3a8f062e732cd9ec9f8c447b930d7
savie <ntop_one@yahoo.co.id>
2026-09-13 19:03:48 +0700

    Route Memory domain UI through runtime authority

--- app/lib/features/journey/memory/memory_view.dart
+++ app/lib/features/journey/memory/memory_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import '../semantic_runtime_domain_view.dart';
 import '../semantic_domain_view.dart';
 
 class MemoryView extends StatelessWidget {
   const MemoryView({super.key});
 
   @override
-  Widget build(BuildContext context) => const SemanticDomainView(domain: ShSemanticDomain.memory);
+  Widget build(BuildContext context) => const SemanticRuntimeDomainView(domain: ShSemanticDomain.memory);
 }
