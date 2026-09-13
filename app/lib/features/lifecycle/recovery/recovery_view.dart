3f8a9d3542927e2b437c860561b20174abdd9350
savie <ntop_one@yahoo.co.id>
2026-09-13 20:56:45 +0700

    Preserve Recovery view contract

--- app/lib/features/lifecycle/recovery/recovery_view.dart
+++ app/lib/features/lifecycle/recovery/recovery_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../lifecycle_models.dart';
 import 'recovery_runtime_view.dart';
 
 class RecoveryView extends StatelessWidget {
   const RecoveryView({super.key, this.incomingItems = const []});
 
-  final List<dynamic> incomingItems;
+  final List<JourneyLifecyclePayload> incomingItems;
 
   @override
   Widget build(BuildContext context) => const RecoveryRuntimeView();
